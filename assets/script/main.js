2cfae5e59a51e8257025c95f5a09d71fbcdea084
amrh-97 <megaart979@gmail.com>
2024-08-05 14:16:16 +0500

    Update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,15 @@ const navMenu = document.querySelector('.mainheader__nav');
 let navShowHide = navIcon.addEventListener('click', () => {
     headContent.classList.toggle('active');
 })
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzqTvQAiHir9hEHrox_CGLPjw0tzuypY8Hq-0Vavk01kVpbdVl84hlEgEKoDtS-k_Mc/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
